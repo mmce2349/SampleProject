e8d685323d5020cd74ed6c386b37544b2660e363
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-01-25 08:37:30 -0700

    added start to controller

--- SampleProject/Controller/Controller.hpp
+++ SampleProject/Controller/Controller.hpp
@@ -9,6 +9,16 @@
 #ifndef Controller_hpp
 #define Controller_hpp
 
-
+class Controller
+{
+private:
+    //data members and helper methods
+protected:
+    // Things that need to only be used by subclasses.
+public:
+    //Anyone can see this.
+    Controller();
+    void start();
+};
 
 #endif /* Controller_hpp */
