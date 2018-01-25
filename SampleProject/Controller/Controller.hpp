b59fdc22a646dabd8c12a79dca81856830313f2c
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-01-25 08:29:24 -0700

    Added a group and controller and runner.

---
+++ SampleProject/Controller/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  SampleProject
+//
+//  Created by Mcentire, Mitchell on 1/25/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+
+
+#endif /* Controller_hpp */
---
+++ SampleProject/Controller/Runner.cpp
@@ -0,0 +1,15 @@
+//
+//  Runner.cpp
+//  SampleProject
+//
+//  Created by Mcentire, Mitchell on 1/25/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+int main()
+{
+    Controller app;
+    app.start();
+}
