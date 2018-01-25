d1cdae635287d014dac9220e91cac933058c2b30
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-01-25 09:31:40 -0700

    Done

--- SampleProject/Controller/Runner.cpp
+++ SampleProject/Controller/Runner.cpp
@@ -7,9 +7,16 @@
 //
 
 #include "Controller.hpp"
-
+using namespace std;
 int main()
 {
     Controller app;
     app.start();
+    
+    cout << "Mitch McEntire" << endl;
+    for (int index = 12; index < 31; index++)
+    {
+        cout << "This loop has executed " << index + 1 << " times" << endl;
+    }
+    
 }
